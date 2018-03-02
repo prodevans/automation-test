fc6c0bef77090d30837dd36a6f077c0f83cfd022
Siddappa Sandur <siddappa@prodevans.com>
2018-03-02 12:21:08 +0530

    updated

--- src/test/java/com/pack/home/TestExecution/ExtentManager.java
+++ src/test/java/com/pack/home/TestExecution/ExtentManager.java
@@ -11,9 +11,9 @@ public class ExtentManager {
 			extent = new ExtentReports(System.getProperty("user.dir") + "/test-output/ZenoExtentReport.html", true);
 			extent.loadConfig(new File(System.getProperty("user.dir") + "\\extent-config.xml"));
 			extent
-			.addSystemInfo("Host Name", "marcus.oneeight.co.in")
+			.addSystemInfo("Host Name", "http://52.172.33.72:8080/ServicePortal")
 			.addSystemInfo("Environment", "DEMO")
-			.addSystemInfo("Application", "Zeno");
+			.addSystemInfo("Application", "ServicePortal");
 		}
 		return extent;
 	}
--- src/test/java/com/pack/home/TestExecution/TestRun.java
+++ src/test/java/com/pack/home/TestExecution/TestRun.java
@@ -20,43 +20,37 @@ public class TestRun {
 		driver = Dlib.openBrowser("chrome");
 		lib = new LibraryForGenericFunction(driver);
 		// Application link will be opened
-		driver.get("http://marcus.oneeight.co.in/zeno/login");
+		driver.get("http://52.172.33.72:8080/ServicePortal");
 	}
 
-	@Test(priority = 1, description="Performs an unsuccessful login and checks the resulting error message (passes)")
+	/*@Test(priority = 1, description="Performs an unsuccessful login and checks the resulting error message (passes)")
 	public void unsuccessfullogin() throws InterruptedException, IOException {
-		lib.jsSendKeysForID("username", "ROL000008", "login", "ID");
-		lib.jsSendKeysForID("password", "123456", "login", "ID");
+		lib.jsSendKeysForID("uname", "anand", "login", "ID");
+		lib.jsSendKeysForID("pass", "123456", "login", "ID");
 		Assert.assertTrue(lib.waitAndClickForID("login", "login", "ID"));
 		Thread.sleep(2000);
-		Assert.assertTrue(lib.getText("error", "login", "ID").contains("login fail"));					
+		Assert.assertTrue(lib.getText("error", "login", "ID").contains("login fail"));	
+		
+	}*/
+	@Test(priority = 2, description="Performs an unsuccessful login and checks the resulting error message (passes)")
+	public void trylogin() throws InterruptedException, IOException {
+		Assert.assertTrue(lib.waitAndClickForID("login1", "login", "ID"));
+		Thread.sleep(2000);
+		Assert.assertTrue(lib.getText("error", "login", "ID").contains("login fail"));	
+		
 	}
 	
 	@Test(priority = 2, description="Performs an successful login and checks the dashboard url (passes)")
 	public void login() throws InterruptedException, IOException {
-		lib.jsSendKeysForID("username", "ROL000007", "login", "ID");
-		lib.jsSendKeysForID("password", "123456", "login", "ID");
+		lib.jsSendKeysForID("username", "anand", "login", "ID");
+		lib.jsSendKeysForID("password", "1234", "login", "ID");
 		Assert.assertTrue(lib.waitAndClickForID("login", "login", "ID"));
 		Thread.sleep(2000);
-		Assert.assertEquals(lib.getCurrentUrl(), "http://marcus.oneeight.co.in/zeno/dashboard");					
+		Assert.assertEquals(lib.getCurrentUrl(), "\r\n" + 
+				"http://52.172.33.72:8080/ServicePortal/success.jsp");					
 	}
 	
-	@Test(priority = 3, description="Checks for the welcome message (passes)")
-	public void verifyWelcomeMesg() throws InterruptedException, IOException {
-		Assert.assertEquals(lib.getText("welcomemesg", "dashboard", "class"), "Welcome VAMSI, to your One8 page");			
-	}	
-	
-	@Test(priority = 4, description="Tries to navigate to Make Payment (fails)")
-	public void clickPayment() throws InterruptedException, IOException {
-		lib.waitAndClickForID("paymentlink", "dashboard", "linktext");
-	}
-
-	@Test(priority = 5, description="Navigates to Profile page (passes)")
-	public void clickProfile() throws InterruptedException, IOException {
-		lib.waitAndClickForID("profilelink", "dashboard", "linktext");
-	}
-
-	@Test(priority = 6, description="Logs out (passes)")
+	@Test(priority = 3, description="Logs out (passes)")
 	public void logout() throws InterruptedException, IOException {
 		lib.waitAndClickForID("logoutlink", "dashboard", "linktext");
 	}
